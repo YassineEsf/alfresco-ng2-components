87a24d5e1d7f2a185da451b62f9734274a9110dc
Denys Vuika <denys.vuika@gmail.com>
2021-02-02 14:52:30 +0000

    Revert "travis workaround"
    This reverts commit d4bf79c9ed59ad7edd7b1d029fb4a2b44d79058e.

--- scripts/ci/job_hooks/before_e2e.sh
+++ scripts/ci/job_hooks/before_e2e.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-echo "====== install to be removed once travis cache is working again ====="
-npm ci
-
-npx @alfresco/adf-cli@alpha scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
+./node_modules/@alfresco/adf-cli/bin/adf-cli scan-env --host "$E2E_HOST" -u "$E2E_ADMIN_EMAIL_IDENTITY" -p "$E2E_ADMIN_PASSWORD_IDENTITY"
 
 # Download built application artifact from S3
 ./scripts/ci/utils/artifact-from-s3.sh -a "$S3_DBP_FOLDER/alfresco-demoshell.tar.bz2" -o "./dist/demo-shell"
